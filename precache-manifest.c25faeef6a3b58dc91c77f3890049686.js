661eefc3ce65c7e872c9ee2875afa8251a1553ee
mirbek <mirbek.25.52@gmail.com>
2021-06-06 17:34:33 +0600

    Updates

---
+++ precache-manifest.c25faeef6a3b58dc91c77f3890049686.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fb9171a2a9f94d15d7b82d71d92189a",
+    "url": "/accordion/index.html"
+  },
+  {
+    "revision": "ea82218924a614062565",
+    "url": "/accordion/static/css/main.6631c5cc.chunk.css"
+  },
+  {
+    "revision": "ab85a117f39d501e282e",
+    "url": "/accordion/static/js/2.40b1d22a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/accordion/static/js/2.40b1d22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea82218924a614062565",
+    "url": "/accordion/static/js/main.5d733d2c.chunk.js"
+  },
+  {
+    "revision": "8bf3a778a198a7a25ab9",
+    "url": "/accordion/static/js/runtime-main.75639503.js"
+  }
+]);
